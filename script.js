64eab4c54e73fe08276f1fb086c18c7ed90e5176
unknown <saritadh74@gmail.com>
2022-03-12 00:36:04 +0200

    api fixed

--- script.js
+++ script.js
@@ -3,15 +3,13 @@ let input = document.querySelector('.search-input');
 let searchButton = document.querySelector('button');
 
 searchButton.addEventListener('click', topicSearch);
-fetch(
-  'https://newsapi.org/v2/everything?q=war&apiKey=c21c8c34fd8e4cf5934920dafbe6a706'
-)
+fetch('https://api.spaceflightnewsapi.net/v3/articles')
   .then((res) => res.json())
   .then((data) => {
-    let articles = data.articles;
-    console.log(articles);
+    // let articles = data.articles;
+    console.log(data);
     //-------------
-    articles.forEach((article) => {
+    data.forEach((article) => {
       let card = document.createElement('div');
       card.classList = 'card';
       container.appendChild(card);
@@ -26,10 +24,10 @@ fetch(
       //------------------------------------------------
       title.textContent = article.title;
       card.appendChild(title);
-      img.src = article.urlToImage;
+      img.src = article.imageUrl;
       imgDiv.appendChild(img);
       content.appendChild(imgDiv);
-      description.textContent = article.description;
+      description.textContent = article.summary;
       descriptionDiv.appendChild(description);
       content.appendChild(descriptionDiv);
       link.textContent = 'Read More';
@@ -43,42 +41,75 @@ fetch(
 function topicSearch(e) {
   e.preventDefault();
   let value = input.value;
-  console.log(value);
   container.textContent = '';
-  fetch(
-    `https://newsapi.org/v2/everything?q=${value}&apiKey=c21c8c34fd8e4cf5934920dafbe6a706`
-  )
+  fetch(`https://api.spaceflightnewsapi.net/v3/articles/${value}`)
     .then((res) => res.json())
     .then((data) => {
-      let articles = data.articles;
-      console.log(articles);
+      // let articles = data.articles;
+      let article = data;
       //-------------
-      articles.forEach((article) => {
-        let card = document.createElement('div');
-        card.classList = 'card';
-        container.appendChild(card);
-        let title = document.createElement('h2');
-        let description = document.createElement('p');
-        let img = document.createElement('img');
-        let content = document.createElement('div');
-        content.classList = 'content';
-        let imgDiv = document.createElement('div');
-        let descriptionDiv = document.createElement('div');
-        let link = document.createElement('a');
-        //------------------------------------------------
-        title.textContent = article.title;
-        card.appendChild(title);
-        img.src = article.urlToImage;
-        imgDiv.appendChild(img);
-        content.appendChild(imgDiv);
-        description.textContent = article.description;
-        descriptionDiv.appendChild(description);
-        content.appendChild(descriptionDiv);
-        link.textContent = 'Read More';
-        link.href = article.url;
-        descriptionDiv.appendChild(link);
-        card.appendChild(content);
-      });
+      let card = document.createElement('div');
+      card.classList = 'card';
+      container.appendChild(card);
+      let title = document.createElement('h2');
+      let description = document.createElement('p');
+      let img = document.createElement('img');
+      let content = document.createElement('div');
+      content.classList = 'content';
+      let imgDiv = document.createElement('div');
+      let descriptionDiv = document.createElement('div');
+      let link = document.createElement('a');
+      //------------------------------------------------
+      title.textContent = article.title;
+      card.appendChild(title);
+      img.src = article.imageUrl;
+      imgDiv.appendChild(img);
+      content.appendChild(imgDiv);
+      description.textContent = article.summary;
+      descriptionDiv.appendChild(description);
+      content.appendChild(descriptionDiv);
+      link.textContent = 'Read More';
+      link.href = article.url;
+      descriptionDiv.appendChild(link);
+      card.appendChild(content);
     })
     .catch((err) => console.log(err));
 }
+
+// searchButton.addEventListener('click', topicSearch);
+// fetch(
+//   'https://newsapi.org/v2/everything?q=war&apiKey=c21c8c34fd8e4cf5934920dafbe6a706'
+// )
+//   .then((res) => res.json())
+//   .then((data) => {
+//     let articles = data.articles;
+//     console.log(articles);
+//     //-------------
+//     articles.forEach((article) => {
+//       let card = document.createElement('div');
+//       card.classList = 'card';
+//       container.appendChild(card);
+//       let title = document.createElement('h2');
+//       let description = document.createElement('p');
+//       let img = document.createElement('img');
+//       let content = document.createElement('div');
+//       content.classList = 'content';
+//       let imgDiv = document.createElement('div');
+//       let descriptionDiv = document.createElement('div');
+//       let link = document.createElement('a');
+//       //------------------------------------------------
+//       title.textContent = article.title;
+//       card.appendChild(title);
+//       img.src = article.urlToImage;
+//       imgDiv.appendChild(img);
+//       content.appendChild(imgDiv);
+//       description.textContent = article.description;
+//       descriptionDiv.appendChild(description);
+//       content.appendChild(descriptionDiv);
+//       link.textContent = 'Read More';
+//       link.href = article.url;
+//       descriptionDiv.appendChild(link);
+//       card.appendChild(content);
+//     });
+//   })
+//   .catch((err) => console.log(err));
